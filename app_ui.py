4c947523ded67ff7428ff0bce239b64491244336
bill-jia <bill.jiazy@gmail.com>
2018-05-21 22:37:16 +0100

    Start writing two-way serial link
    Incorrect display of Arduino output on computer - line cut off

--- app_ui.py
+++ app_ui.py
@@ -1,18 +1,47 @@
 import wx
 import serial
+import threading
+import time
+
+testCase = 1
+oesophagusLength = 30
 
 testSignal = "99"
 startSignal = "1"
 endSignal = "-1"
 COMport = 'COM5'
 
+def writeStartSignal():
+	# Write formatted data containing the test case and physical parameters
+	return 0
+
+def initializeSerialLink(COMport):
+	serialLink = serial.Serial(COMport, 9600, timeout=1)
+	return serialLink
+
+def readSerialData(serialLink, event):
+	event.wait()
+	while event.isSet():
+		try:
+			print("Attempt to Read")
+			readOut = serialLink.readline().decode('ascii')
+			time.sleep(1)
+			print(readOut)
+		except:
+			pass
+
 class AppFrame(wx.Frame):
-	def __init__(self, *args, **kw):
+	def __init__(self, serialLink, *args, **kw):
 		# Initialize superclass
+		print(*args)
 		super(AppFrame, self).__init__(*args, **kw)
-		
-		# Initialize serial link
-		self.serialLink = serial.Serial(COMport, 9600, timeout=1)
+
+		# assign serial link
+		self.serialLink = serialLink
+
+		# Initialize event signalling
+		self.receivingData = threading.Event()
+		self.initializeDataListening()
 
 		#Create a panel
 		panel = wx.Panel(self)
@@ -23,16 +52,27 @@ class AppFrame(wx.Frame):
 		self.Bind(wx.EVT_BUTTON, self.OnClickStart, self.startButton)
 		self.Bind(wx.EVT_BUTTON, self.OnClickStop, self.stopButton)
 
+	def initializeDataListening(self):
+		self.dataListeningThread = threading.Thread(name="data-listening", target = readSerialData, args=(serialLink, self.receivingData))
+		self.dataListeningThread.start()
+
 	def OnClickStart(self, event):
 		self.serialLink.write(startSignal.encode("utf-8"))
+		self.receivingData.set()
 
 	def OnClickStop(self, event):
 		self.serialLink.write(endSignal.encode("utf-8"))
+		self.receivingData.clear()
+		if not (self.dataListeningThread.is_alive()):
+			self.initializeDataListening()
+
+
 
 if __name__ == '__main__':
 	uiApp = wx.App()
-	frame = AppFrame(None, title="Cytosponge Training")
+	serialLink = initializeSerialLink(COMport)
+	frame = AppFrame(serialLink, None, title="Cytosponge Training")
 	frame.Show()
 	uiApp.MainLoop()
-	#serialLink = serial.Serial(COMport, 9600, timeout=5)
+	
 	
--- serial_communication_test/serial_communication_test.ino
+++ serial_communication_test/serial_communication_test.ino
@@ -1,3 +1,6 @@
+int i = 0;
+int state = -1;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,11 +8,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int state = Serial.parseInt();
+  int temp_state = Serial.parseInt();
+  if (temp_state == 1) {
+    state = 1;
+    //Serial.write("Hello Arduino");
+  }
+  else if (temp_state == -1) {
+    state = -1;
+  }
   if (state == 1) {
     digitalWrite(4, 1);
+    Serial.write("Hello Raspberry Pi " + i);  
   }
   else if (state == -1) {
     digitalWrite(4,0);
   }
+  
+  i++;
+  delay(500);
 }
