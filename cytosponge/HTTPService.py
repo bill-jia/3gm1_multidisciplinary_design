3a073e983415bc2a004b0747361e60de99b0117b
bill-jia <bill.jiazy@gmail.com>
2018-05-24 08:31:54 +0100

    Lay out structure for l2s2 communication

---
+++ cytosponge/HTTPService.py
@@ -0,0 +1,7 @@
+import cytosponge
+import requests
+import io
+
+class HTTPService:
+	def __init__(self):
+		self.placeholder = 0
--- cytosponge/__init__.py
+++ cytosponge/__init__.py
@@ -3,3 +3,4 @@ from .SerialCommunicationService import SerialCommunicationService
 from .EventService import EventService
 from .CommandService import CommandService
 from .DataService import DataService
+from .HTTPService import HTTPService
